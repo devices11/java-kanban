e09f0b83c98c2a91a50d379067901b40dd40d9d8
Izotov Valentin <vizotov@vtb.ru>
2024-09-02 20:44:54 +0300

    Обновлены тесты InMemoryHistoryManagerTest

--- src/test/service/InMemoryHistoryManagerTest.java
+++ src/test/service/InMemoryHistoryManagerTest.java
@@ -3,96 +3,81 @@ package test.service;
 import main.models.Epic;
 import main.models.Subtask;
 import main.models.Task;
-import main.service.Managers;
-import main.service.TaskManager;
+import main.service.InMemoryHistoryManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryHistoryManagerTest {
-    private TaskManager taskManager;
+    private InMemoryHistoryManager historyManager;
+    Task task1;
+    Epic epic1;
+    Subtask subtask1;
 
     @BeforeEach
     public void beforeEach() {
-        taskManager = Managers.getDefault();
+        historyManager = new InMemoryHistoryManager();
 
-        Task task1 = new Task("Название таски 1", "Описание таски 1");
-        taskManager.createTask(task1);
-
-        Epic epic1 = new Epic("Название эпика 1", "Описание эпика 1");
-        taskManager.createEpic(epic1);
-
-        Subtask subtask1 = new Subtask("Название сабтаски 1", "Описание сабтаски 1", 2);
-        taskManager.createSubtask(subtask1);
+        task1 = new Task("Название таски 1", "Описание таски 1");
+        epic1 = new Epic("Название эпика 1", "Описание эпика 1");
+        subtask1 = new Subtask("Название сабтаски 1", "Описание сабтаски 1", 2);
     }
 
     @Test
     public void add1TaskInHistory() {
-        taskManager.getTaskById(1);
-        assertEquals(1, taskManager.getHistory().size(), "В истории не 1 объект");
-    }
-
-    @Test
-    public void viewNonExistentTask() {
-        taskManager.getTaskById(1);
-        taskManager.getTaskById(111);
-        assertEquals(1, taskManager.getHistory().size(), "В истории не 1 объект");
-        assertEquals(1, taskManager.getHistory().getFirst().getId(), "id не совпадает");
+        historyManager.add(task1);
+        assertEquals(1, historyManager.getHistory().size(), "В истории не 1 объект");
     }
 
     @Test
     public void taskInHistoryAllVersion() {
-        taskManager.getTaskById(1);
+        historyManager.add(task1);
         Task taskForUpdate = new Task("Название таски 2", "Описание таски 2");
         taskForUpdate.setId(1);
-        taskManager.updateTask(taskForUpdate);
-        taskManager.getTaskById(1);
-        assertEquals(2, taskManager.getHistory().size(), "В истории не 2 объекта");
-        assertEquals("Название таски 1", taskManager.getHistory().getFirst().getTitle());
-        assertEquals("Описание таски 1", taskManager.getHistory().getFirst().getDescription());
-        assertEquals("Название таски 2", taskManager.getHistory().get(1).getTitle());
-        assertEquals("Описание таски 2", taskManager.getHistory().get(1).getDescription());
+        historyManager.add(taskForUpdate);
+        assertEquals(2, historyManager.getHistory().size(), "В истории не 2 объекта");
+        assertEquals("Название таски 1", historyManager.getHistory().getFirst().getTitle());
+        assertEquals("Описание таски 1", historyManager.getHistory().getFirst().getDescription());
+        assertEquals("Название таски 2", historyManager.getHistory().get(1).getTitle());
+        assertEquals("Описание таски 2", historyManager.getHistory().get(1).getDescription());
     }
 
-    @Test
+        @Test
     public void add10TaskInHistory() {
         //Посмотрели 10 объектов
-        taskManager.getTaskById(1);
-        taskManager.getTaskById(1);
-        taskManager.getTaskById(1);
-        taskManager.getEpicById(2);
-        taskManager.getEpicById(2);
-        taskManager.getEpicById(2);
-        taskManager.getSubtaskById(3);
-        taskManager.getSubtaskById(3);
-        taskManager.getSubtaskById(3);
-        taskManager.getSubtaskById(3);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(epic1);
+        historyManager.add(subtask1);
 
-        assertEquals(10, taskManager.getHistory().size(), "В истории не 10 объектов");
+        assertEquals(10, historyManager.getHistory().size(), "В истории не 10 объектов");
     }
 
     @Test
     public void checkLimit10TaskInHistory() {
         //Посмотрели 11 объектов
-        taskManager.getEpicById(2);
-        taskManager.getTaskById(1);
-        taskManager.getEpicById(2);
-        taskManager.getTaskById(1);
-        taskManager.getEpicById(2);
-        taskManager.getEpicById(2);
-        taskManager.getSubtaskById(3);
-        taskManager.getSubtaskById(3);
-        taskManager.getTaskById(1);
-        taskManager.getEpicById(2);
-        taskManager.getSubtaskById(3);
+        historyManager.add(task1);
+        historyManager.add(epic1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(task1);
+        historyManager.add(epic1);
+        historyManager.add(subtask1);
+        historyManager.add(task1);
 
-        assertEquals(10, taskManager.getHistory().size(),
-                "В истории не 10 объектов");
-        assertEquals(1, taskManager.getHistory().getFirst().getId(),
-                "Первый не с id = 1");
-        assertEquals(3, taskManager.getHistory().get(9).getId(),
-                "Десятый объект в истории не с id = 3");
+        assertEquals(10, historyManager.getHistory().size(),"В истории не 10 объектов");
+        assertInstanceOf(Epic.class, historyManager.getHistory().getFirst(), "Первый объект не Epic");
+        assertInstanceOf(Epic.class, historyManager.getHistory().getFirst());
+        assertInstanceOf(Task.class, historyManager.getHistory().get(9),"Десятый объект не Task");
     }
 }
