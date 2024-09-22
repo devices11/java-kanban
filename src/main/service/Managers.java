86da85201dc1513e940bd0f66f959dde987908ef
Izotov Valentin <vizotov@vtb.ru>
2024-09-22 15:38:10 +0300

    1. Убран публичный конструктор для утилитарного Managers

--- src/main/service/Managers.java
+++ src/main/service/Managers.java
@@ -1,6 +1,9 @@
 package main.service;
 
 public class Managers {
+
+    private Managers() {}
+
     public static TaskManager getDefault() {
         return new InMemoryTaskManager(getDefaultHistory());
     }
